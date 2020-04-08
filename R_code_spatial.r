ba93ea7a7b36e89e1e67fd8bb552b4cc2361bb8a
francescodematte <62662007+francescodematte@users.noreply.github.com>
2020-04-08 18:03:12 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -71,3 +71,46 @@ ggplot(mpg,aes(x=displ, y=hwy))+geom_polygon()
 head(covid)
 
 ggplot(covid,aes(x=lon, y=lat, size=cases))+geom_point()
+
+setwd("C:/lab/")
+
+load("point_pattern.RData")
+
+ls()
+
+#covids:point pattern
+#d:density map
+
+library(spatstat)
+
+plot(d)
+
+points(covids)
+
+library(rgdal)
+#let s input vector lines
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+cl <- colorRampPalette(c("yellow","orange","red")) (100)
+plot(d, col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+EX:new colour ramp palette
+
+cl <- colorRampPalette(c("light green","yellow","orange")) (100)
+plot(d, col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+cl <- colorRampPalette(c("light blue","yellow","orange")) (100)
+plot(d, col=cl, main= "Densities of Covid-19")   #here the title is changed with the command "main"
+points(covids)
+plot(coastlines, add=T)
+
+pdf("covid_density.pdf")
+clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
+plot(d, col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+dev.off()   #exported in lab as a pdf file
