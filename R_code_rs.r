27b519575e0cab765873527b64abb2dcce3122c0
francescodematte <62662007+francescodematte@users.noreply.github.com>
2020-04-24 18:04:29 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -48,3 +48,75 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")  #bare areas in yellow
  
 
+setwd("~/lab/")
+load("rs.RData")
+ls()
+
+library(raster)
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+plot(p224r63_1988)
+
+#exercise: plot in RGB (1988/2011)
+
+par(mfrow=c(2,1))
+
+
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+#exercise: now remove the blue band and replace it with NIR one (false color)
+
+par(mfrow=c(2,1))
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+
+#enhance the noise
+
+par(mfrow=c(2,1))
+
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+par(mfrow=c(2,1))
+
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+cl <- colorRampPalette(c("darkorchid3",'light blue','lightpink4'))(100) 
+plot(dvi2011)
+ 
+#exercise:dvi in 1988
+
+dvi1988 <-p224r63_1988 $B4_sre - p224r63_1988$B3_sre
+cl <- colorRampPalette(c("darkorchid3",'light blue','lightpink4'))(100) 
+plot(dvi1988)
+
+#difference from one year to the other
+
+diff <- dvi2011 - dvi1988
+plot(diff)
+
+#choosing a different grain (resampling)
+
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res100, r=4, g=3, b=2, stretch="Lin")
+ 
+
+
+
+
+
+
+
+
