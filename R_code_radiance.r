bbe9e8ff6f0ce91f97860037a235a25555909f6e
francescodematte <62662007+francescodematte@users.noreply.github.com>
2020-05-15 17:07:35 +0200

    Update R_code_radiance.r

--- R_code_radiance.r
+++ R_code_radiance.r
@@ -2,28 +2,24 @@ library(raster)
 
 toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)
  
- values(toy) <- c(1.13,1.44,1.55,3.4)
+values(toy) <- c(1.13,1.44,1.55,3.4)
  
- plot(toy)
+plot(toy)
  
- text(toy, digits=2)
+text(toy, digits=2)
  
- toy2bits <- stretch(toy,minv=0,maxv=3)
+toy2bits <- stretch(toy,minv=0,maxv=3)
  
- storage.mode(toy2bits[]) = "integer"
+storage.mode(toy2bits[]) = "integer"
  
- plot(toy2bits)
+plot(toy2bits)
  
- text(toy2bits, digits=2)
+text(toy2bits, digits=2)
  
+toy2bits <- stretch(toy,minv=0,maxv=3)
  
-    toy2bits <- stretch(toy,minv=0,maxv=3)
+storage.mode(toy2bits[]) = "integer"
  
-
-    storage.mode(toy2bits[]) = "integer"
- 
-
-    
 plot(toy2bits)
 text(toy2bits, digits=2)
 
@@ -39,16 +35,12 @@ toy8bits <- stretch(toy,minv=0,maxv=255)
 
 storage.mode(toy8bits[]) = "integer"
 
- 
-
 plot(toy8bits)
-
 text(toy8bits, digits=2)
 
 par(mfrow=c(1,4))
 
  
-
 plot(toy)
 text(toy, digits=2)
 
