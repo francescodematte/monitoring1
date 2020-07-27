191313e0bde8ea04ad093555722640783a7a583f
francescodematte <62662007+francescodematte@users.noreply.github.com>
2020-07-27 18:29:00 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -899,3 +899,115 @@ cl <- colorRampPalette(c('yellow','violet','black'))(100) #
 plotRGB(clad, 1,2,3, stretch="lin")
 plot(sd_clad, col=cl)
 # plot(sd_clad_agg, col=cl)
+
+
+
+#13_R_code_snow.r#################################################################################
+####################################################################################
+# R_code_snow.r
+
+#1st set the working directory to the folder snow 
+#we create another folder in C, outside the folder lab, because we will use the function lapply() to search files that comprehend the name within that folder.
+#in this case we don't have other files named snow but it's better to always mind about this
+setwd("C:/snow/")
+
+#we will use raster library and ncdf4(interface with netcdf data, some sort of .tif). Since most of the copernicus are based on nCDF we need to import this library 
+
+install.packages("ncdf4")
+
+library(ncdf4)  #we can now recall the library
+library(raster)
+
+#we import our file and name it snowmay
+snowmay <-raster("c_gls_SCE_202005280000_NHEMI_VIIRS_V1.0.1.nc")
+cl <- colorRampPalette(c('darkblue','blue','light blue'))(100) 
+
+
+#### Exercise: plot snow cover with the cl palette ####
+plot(snowmay,col=cl)
+
+#one simple but very slow manner to import the set is: raster("snow2000r.tif") and associate it to the vector snow2000 and so on for every year
+snow2000 <- raster("snow2000r.tif")
+snow2005 <- raster("snow2005r.tif")
+snow2010 <- raster("snow2010r.tif")
+snow2015 <- raster("snow2015r.tif")
+snow2020 <- raster("snow2020r.tif")
+
+#we can at this point use the par
+
+par(mfrow=c(2,3)) #we cannot do 1,5 because it's too many graphs in one row)
+plot(snow2000,col=cl)
+plot(snow2005,col=cl)
+plot(snow2010,col=cl)
+plot(snow2015,col=cl)
+plot(snow2020,col=cl)
+#this is one of the ways to import the graphs but implies many lines of code to be written!
+
+#the next function gives us a much a quicker way to import all the images
+
+# lapply() is useful to apply a function to a list of vectors
+#first of all we need to make the list of files that we want to use
+#the function is list.files, it will only make the list of all the files with a certain shared pattern in a certain place
+#in our case our files all include snow20 in the name
+rlist <- list.files(pattern="snow20")
+#than just write rlist to see the files within it
+
+#we can now make the stack function to connect the images on layers
+import <- lapply(rlist,raster)
+snow.multitemp <- stack(import)
+
+#launch snow.multitemp to see the object
+#at this point we can plot the whole vector snow.multitemp with the col=cl
+plot(snow.multitemp, col=cl)
+ 
+
+#we can now operate applying a linear regression to see the tendency of the reduction of snow during the years and predict the value for 2025
+
+####prediction####
+
+require(raster)   #command very similar to library
+require(rgdal)
+
+# define the extent: 180=extent of data, 90=extent of latitude
+ext <- c(-180, 180, -90, 90)
+extension <- crop(snow.multitemp, ext)
+    
+# make a time variable (to be used in regression)
+time <- 1:nlayers(snow.multitemp)
+
+# run the regression
+fun <- function(x) {if (is.na(x[1])){ NA } else {lm(x ~ time)$coefficients[2] }} 
+predicted.snow.2025 <- calc(extension, fun) # this calculation is quite time consuming
+predicted.snow.2025.norm <- predicted.snow.2025*255/53.90828
+#255 because 8bit means 2 elevated8 -> 256 different informations (interval between 0 and 255)
+###############################################
+#we take all the previous prediction saving the file prediction.r in the folder snow, launching the code source("prediction.r")
+source("prediction.r")
+
+#save raster into list with lapply
+
+list_rast <- lapply(rlist, raster)
+snow.multitemp <- stack(list_rast)
+plot(snow.multitemp,col=cl)
+
+par(mfrow=c(1,2))
+plot(snow.multitemp$snow2000r, col=cl)
+plot(snow.multitemp$snow2020r, col=cl)
+
+par(mfrow=c(1,2))
+plot(snow.multitemp$snow2000r, col=cl, zlim=c(0,250))   #zlim=	the minimum and maximum z values for which colors should be plotted, defaulting to the range of the finite values of z
+plot(snow.multitemp$snow2020r, col=cl, zlim=c(0,250))
+
+difsnow = snow.multitemp$snow2020r - snow.multitemp$snow2000r
+cldiff <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difsnow, col=cldiff)
+
+# prediction
+# go to IOL and downloand prediction.r into the folder snow
+# source("prediction.r")
+# plot(predicted.snow.2025.norm, col=cl)
+# since the code needs time, you can download predicted.snow.2025.norm.tif from iol in the Data
+
+predicted.snow.2025.norm <- raster("predicted.snow.2025.norm.tif")
+plot(predicted.snow.2025.norm, col=cl)
+
